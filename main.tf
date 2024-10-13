adfbafa8ff99328232584e84ea95f8a6db909a24
luminosita <milosh@emisia.net>
2024-10-13 02:00:38 +0200

    Snapshot

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
+data "hcloud_ssh_key" "default" {
+  name = "Gianni"
+}
+
 resource "hcloud_ssh_key" "default" {
+  count = data.hcloud_ssh_key.default.public_key == null ? 1 : 0
+
   name       = "Gianni"
-  public_key = file(var.root_ssh_public_key_file)
+  public_key = file(var.hcloud.ssh_public_key_file)
 }
 
 resource "hcloud_server" "vm" {
   for_each = toset(distinct([for k, v in var.images : k]))
 
   name        = each.key
-  image       = var.os.vm_base_image
+  image       = var.os.vm_base_image != null ? var.os.vm_base_image : var.os.vm_snapshot_id
   datacenter  = var.images[each.key].vm_location
   server_type = var.images[each.key].vm_server_type
   public_net {
@@ -15,13 +21,12 @@ resource "hcloud_server" "vm" {
     ipv6_enabled = true
   }
 
-  ssh_keys = [ hcloud_ssh_key.default.id ]
+  ssh_keys = [ data.hcloud_ssh_key.default.public_key == null ? hcloud_ssh_key.default[0].id : data.hcloud_ssh_key.default.id ]
 
-  user_data = templatefile("${path.module}/resources/cloud-init/vm-init.yaml.tftpl", {
+  user_data = var.images[each.key].vm_cloud_init ? templatefile("${path.module}/resources/cloud-init/vm-init.yaml.tftpl", {
     hostname      = each.key
     username      = var.images[each.key].vm_user
     pub-keys      = var.images[each.key].vm_ssh_public_key_files
     run-cmds      = var.images[each.key].vm_run_cmds
-  })
+  }) : null
 }
-
--- variables.tf
+++ variables.tf
@@ -1,11 +1,24 @@
-variable "root_ssh_public_key_file" {
-  type = string
+variable "hcloud" {
+    type        = object({
+        ssh_public_key_file    = string
+    })
 }
 
 variable "os" { 
     type = object({
-        vm_base_image = string
+        vm_base_image = optional(string)
+        vm_snapshot_id = optional(number)
     })
+
+    validation {
+        condition     = var.os.vm_base_image != null || var.os.vm_snapshot_id != null
+        error_message = "vm_base_image or vm_snapshot_id must be specified"
+    }
+
+    # validation {
+    #     condition     = try(var.os.vm_snapshot_id == null, var.os.vm_snapshot_id > 0)
+    #     error_message = "vm_snapshot_id must be greater than 0"
+    # }
 }
 
 variable "images" {
@@ -14,9 +27,11 @@ variable "images" {
         vm_location     = string
         vm_server_type  = string
 
-        vm_user         = string
-        vm_ssh_public_key_files = list(string)
+        vm_cloud_init   = bool
 
-        vm_run_cmds     = list(string)
+        vm_user         = optional(string)
+        vm_ssh_public_key_files = optional(list(string))
+
+        vm_run_cmds     = optional(list(string))
     }))
 }
